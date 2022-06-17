80baa17445f7f1b9945cdc8c1674ee0576541c1a
Pedro <92195721+PedroHenriqueMR@users.noreply.github.com>
2022-06-16 21:46:20 -0300

    Update Calculadora.js

--- Calculadora.js
+++ Calculadora.js
@@ -12,69 +12,69 @@ if(continua == "1"){
                 if(numero1 != Number(numero1)){
                     console.log("Somente numeros por gentileza.")
                 }else{
+
+                    let numero2 = calculadora.question("\nDígite o segundo número que você desesa calcular: ");
+
+                    if(numero2 != Number(numero2)){
+
+                        console.log("\nSomente numeros por gentileza.\n")
+                        break;
+
+                    }else{
                         console.log("\nQual dos calculos abaixo voce gostaria de fazer?...");
                         console.log("+ - Soma");
                         console.log("- - Subtração");
                         console.log("* - Multiplicação");
                         console.log("/ - Divisão\n");
-
+    
                         let calculo = calculadora.question(" ");
-
-
-                        if(calculo !== String(calculo = "+" || "-" || "*" || "/")){
-
-                            console.log("\nOpção invalida!")
-
-                        }else{
-
-                            let numero2 = calculadora.question("\nDígite o segundo número que você desesa calcular: ");
-
-                            if(numero2 != Number(numero2)){
-
-                                console.log("\nSomente numeros por gentileza.\n")
-
-                            }else{
-                                                
-                                switch(calculo){
-                                    case "+": {
-                                            let soma = Number(numero1) + Number(numero2);
-                                            console.log("\nResultado: "+numero1+" + "+numero2+" = "+soma+"\n");
-                                            break;
-                                    }
-                                    case "-": {
-                                            let subtracao = numero1 - numero2
-                                            console.log("\nResultado: "+numero1+" - "+numero2+" = "+subtracao+"\n");
-                                            break;
-                                    }
-                                    case "*": {
-                                            let multiplicacao = numero1 * numero2
-                                            console.log("\nResultado: "+numero1+" * "+numero2+" = "+multiplicacao+"\n");
-                                            break;
-                                    }
-                                    case "/": {
-                                        if(numero1 == "0" || numero2 == "0"){
-                                            console.log("\nNão tem como dividir por 0!")
-                                            break;
-                                        }else{
-                                            let multiplicacao = numero1 * numero2
-                                            console.log("\nResultado: "+numero1+" * "+numero2+" = "+multiplicacao+"\n");
-                                        };
-                                    };
-                                };
-
-                                let continuar =  calculadora.question("\nSé você deseja calcular digite 1 "+"\n"+"Sé você deseja encerar digite 2\n");
-                                if(numero1 != Number(numero1)){
-                                    console.log("Somente numeros por gentileza.")
-                                }else{
-                                    if(continuar == 1){
-                                        console.log("\n"+nome+", você optou por continuar.")
-                                    }else if(continuar == 2) {
-                                    console.log("Obrigador por participar "+nome)
+    
+                        if(calculo){
+                            switch(calculo){
+                                case "+": {
+                                    let soma = Number(numero1) + Number(numero2);
+                                    console.log("\nResultado: "+numero1+" + "+numero2+" = "+soma+"\n");
                                     break;
-                                } 
+                                }
+                                case "-": {
+                                    let subtracao = numero1 - numero2
+                                    console.log("\nResultado: "+numero1+" - "+numero2+" = "+subtracao+"\n");
+                                    break;
+                                }
+                                case "*": {
+                                    let multiplicacao = numero1 * numero2
+                                    console.log("\nResultado: "+numero1+" * "+numero2+" = "+multiplicacao+"\n");
+                                    break;
+                                }
+                                case "/": {
+                                    if(numero1 == "0" || numero2 == "0"){
+                                        console.log("\nNão tem como dividir por 0!")
+                                        break;
+                                    }else{
+                                        let multiplicacao = numero1 * numero2
+                                        console.log("\nResultado: "+numero1+" * "+numero2+" = "+multiplicacao+"\n");
+                                    };
+                                    break;
+                                };
+                                default:{
+                                    console.log("Opção invalida")
+                                    break;
+                                }
+    
                             }
-                        } 
+                        }
                     }
+                }
+                    let continuar =  calculadora.question("\nSé você deseja calcular digite 1 "+"\n"+"Sé você deseja encerar digite 2\n");
+                    if(numero1 != Number(numero1)){
+                        console.log("Somente numeros por gentileza.")
+                    }else{
+                        if(continuar == 1){
+                            console.log("\n"+nome+", você optou por continuar.")
+                        }else if(continuar == 2) {
+                        console.log("Obrigador por participar "+nome)
+                        break;
+                    } 
                 }
             }
     }else if(continua == "2"){
